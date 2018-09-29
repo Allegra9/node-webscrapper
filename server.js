6ccca9a6f749072aeaa8b272eb0410c0171f3ff5
Allegra <aukseallegra@gmail.com>
2018-09-30 00:19:25 +0100

    added Promise.all, and validation functions

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const axios = require('axios');
-// require('make-runnable');
 
 // hackernews --posts n
 // --posts how many posts to print. A positive integer <= 100.
-//  hackernews( {posts: n} )  //func that takes in an arg obj
+//  hackernews({ posts: n })
 
+//  node server hackernews --posts 5
 
 // module.exports = {
 //     addTogether: function(x,y){
@@ -19,39 +19,28 @@ const axios = require('axios');
 // require('make-runnable');
 
 
-module.exports = {
-    hackernews: async function(obj){
-      try {
-        const res1 = await axios.get('https://news.ycombinator.com/news?p=1')
-        const res2 = await axios.get('https://news.ycombinator.com/news?p=2')
-        const res3 = await axios.get('https://news.ycombinator.com/news?p=3')
-        const res4 = await axios.get('https://news.ycombinator.com/news?p=4')
-        let htmls = [res1.data, res2.data, res3.data, res4.data]  // 4
-        for (let i = 0; i < htmls.length; i++){
-          getPosts(htmls[i], obj.posts)
-          //return getPosts(htmls[i], obj.posts)  // only first 30
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    }
-};
-require('make-runnable');
-
-// const hackernews = async ({posts: postsRequested}) => {
-//   try {
-//     const res1 = await axios.get('https://news.ycombinator.com/news?p=1')
-//     const res2 = await axios.get('https://news.ycombinator.com/news?p=2')
-//     const res3 = await axios.get('https://news.ycombinator.com/news?p=3')
-//     const res4 = await axios.get('https://news.ycombinator.com/news?p=4')
-//     let htmls = [res1.data, res2.data, res3.data, res4.data]  // 4
-//     for (let i = 0; i < htmls.length; i++){
-//       return getPosts(htmls[i], postsRequested)
+// module.exports = {
+//     hackernews: async function(obj){
+//       //LALALALa
 //     }
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+// };
+// require('make-runnable');
+
+const hackernews = async ({posts: postsRequested}) => {
+  try {
+    let htmls = await Promise.all([
+      axios.get('https://news.ycombinator.com/news?p=1'),
+      axios.get('https://news.ycombinator.com/news?p=2'),
+      axios.get('https://news.ycombinator.com/news?p=3'),
+      axios.get('https://news.ycombinator.com/news?p=4')
+    ])
+    for (let i = 0; i < htmls.length; i++){
+      getPosts(htmls[i].data, postsRequested)
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 const getPosts = (html, posts) => {
   let results = []
@@ -70,11 +59,11 @@ const getPosts = (html, posts) => {
     let comments = $(subtext).eq(5).text()
 
     let obj = {
-      title: title,
-      uri: uri,
-      author: author,
-      points: parseInt(points),
-      comments: parseInt(comments),
+      title: checkInput(title),
+      uri: checkURI(uri),
+      author: checkInput(author),
+      points: checkPoints(points),
+      comments: checkComments(comments),
       rank: parseInt(rank)
     }
     if (obj.rank <= posts) {
@@ -87,29 +76,40 @@ const getPosts = (html, posts) => {
   }
 }
 
-//hackernews({posts: 2})  // will be called from terminal
+hackernews({posts: 32})  // will be called from terminal
 
 //VALIDATIONS:
 
-// title and author are non empty strings not longer than 256 characters.
-// title.length > 0 && title.length < 256 ?
-//   title
-//   : "no title or it's wayyyy too long"  //title = title.substring(0,253)+"...";
-//
-// author.length > 0 && author.length < 256 ?
-//   author
-//   : "no author or it's wayyyy too long"
-//
-// //points and comments are integers >= 0
-// if (comments === 'discuss' || parseInt(comments) <= 0) {
-//   comments = 0
-// }
-// if (parseInt(points) < 0) {
-//   points = 0
-// }
-// //checks if uri is valid:
-//  let regex = (^(([^:\/?#]+):)?(\/\/([^\/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?)
-// let regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
-// if (!regex.test(uri)){
-//   uri = "URI NOT VALID !!!!!!!!!!!"
-// }
+const checkInput = (input) => {
+  if (input.length > 0 && input.length < 256){
+    return input
+  }else {
+    return 'no input or its more than 256 chars'
+    //input = input.substring(0,253)+"...";
+  }
+}
+
+const checkURI = (uri) => {
+  let regex = /(^(([^:\/?#]+):)?(\/\/([^\/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?)/
+  if (regex.test(uri)){
+    return uri
+  }else {
+    return "uri not valid"
+  }
+}
+
+const checkPoints = (points) => {
+  if (parseInt(points) <= 0) {
+    return 0
+  }else {
+    return parseInt(points)
+  }
+}
+
+const checkComments = (comments) => {
+  if (comments === 'discuss' || parseInt(comments) <= 0) {
+    return 0
+  }else {
+    return parseInt(comments)
+  }
+}
